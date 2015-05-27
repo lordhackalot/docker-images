888d68073402902a30c89a9715f154e1f2852c53
nattapon <nattapon.v@kaidee.com>
2015-05-28 01:14:40 +0700

    fix elasticsearch behind docker nat issue

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
-echo "${ELASTICNODE01}" >  /result.txt
-sed -i -e 's/\[\[NODENAME\]\]/'"${ELASTIC_NODE01}"'/g' /someconfig.txt
 export PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin"
+echo "${ELASTICNODE}" >  /nodename
+sed -i -e 's/\[\[ELASTICNODE\]\]/'"${ELASTICNODE}"'/g' /etc/elasticsearch/elasticsearch.yml
+sed -i -e 's/\[\[INST_ADDR\]\]/'"${INST_ADDR}"'/g' /etc/elasticsearch/elasticsearch.yml
+sed -i -e 's/\[\[INST_ADDR01\]\]/'"${INST_ADDR01}"'/g' /etc/elasticsearch/elasticsearch.yml
+sed -i -e 's/\[\[INST_ADDR02\]\]/'"${INST_ADDR02}"'/g' /etc/elasticsearch/elasticsearch.yml
+sed -i -e 's/\[\[INST_ADDR03\]\]/'"${INST_ADDR03}"'/g' /etc/elasticsearch/elasticsearch.yml
 /sbin/service elasticsearch start
 while true; do
  if lsof -ni:9200 ; then
@@ -13,5 +17,4 @@ while true; do
   sleep 2
 done
 curl -XPUT -u es_admin:redhat 'http://localhost:9200/_licenses' -d @/etc/elasticsearch/shield/license.json
-#tail -f /var/log/elasticsearch/elasticsearch.log
 tail -f /dev/null
